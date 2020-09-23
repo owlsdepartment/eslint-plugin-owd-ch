f603a613942ea734eabd137b3b3b8fb47c008cf1
Bartosz Jędrasik <arcybartek@gmail.com>
2020-09-23 11:46:30 +0200

    fixed wording

--- data/index.js
+++ data/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   ATTR_TESTID: 'data-testid',
-  ERROR_MSG_MISSING_TESTID_KEY: 'interactive elements must contain a `data-testid` attribute or prop.',
+  ERROR_MSG_MISSING_TESTID_KEY: 'Interactive elements must contain a `data-testid` attribute or prop.',
   ERROR_MSG_MISSING_TESTID_VALUE: '`data-testid` cannot be empty.'
 }
