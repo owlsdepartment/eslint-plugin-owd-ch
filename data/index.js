5fdf73ad9666fb34a96ae71748a8f2922b340fbd
Bartosz Jędrasik <arcybartek@gmail.com>
2020-09-29 14:12:34 +0200

    typo

--- data/index.js
+++ data/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   ATTR_TESTID: 'data-testid',
-  PROP_TESTID: 'dataTestId',
+  PROP_TESTID: 'dataTestid',
   ERROR_MSG_MISSING_TESTID_KEY: 'Interactive elements must contain a `data-testid` attribute or prop.',
   ERROR_MSG_MISSING_TESTID_VALUE: '`data-testid` cannot be empty.'
 }
